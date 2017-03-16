102ee03fd340dc1d5d08555e8db0e26af3d38c45
David Siro <david@catchblock.me>
2017-03-16 17:46:46 +0100

    computing trend & simplified reducer

---
+++ src/app/models/trend.ts
@@ -0,0 +1,3 @@
+export enum Trend {
+    UP, DOWN
+}
--- src/app/reducers/tick.ts
+++ src/app/reducers/tick.ts
@@ -2,33 +2,47 @@ import * as trades from "../actions/tick";
 import {Tick} from "../models/tick";
 import {ActionTypes} from "../actions/tick";
 import * as connection from "../actions/connection";
+import {Trend} from "../models/trend";
 
 export interface State {
     ticks: Tick[];
+    trend: Trend;
 }
 
 export const initialState: State = {
-    ticks: []
+    ticks: [],
+    trend: null
 };
 
 export function reducer(state = initialState, action: trades.Actions): State {
     switch (action.type) {
         case ActionTypes.TICK_RECEIVED:
-            let noOfTicks = state.ticks.length;
-            if (noOfTicks < 10) {
-                return {
-                    ticks: [action.payload, ...state.ticks]
-                };
-            } else {
-                return {
-                    ticks: [action.payload, ...state.ticks.slice(0, noOfTicks - 1)]
-                };
+            const noOfTicks = state.ticks.length;
+            const endIndex = noOfTicks < 10 ? noOfTicks : noOfTicks - 1;
+            const latestTick = action.payload;
+            return {
+                ticks: [latestTick, ...state.ticks.slice(0, endIndex)],
+                trend: computeTrend(latestTick, state.ticks[0], state.trend)
             }
         case connection.ActionTypes.DISCONNECT_SUCCESS:
             return {
-                ticks: []
+                ticks: [],
+                trend: null
             };
         default:
             return state;
     }
 }
+
+function computeTrend(current: Tick, previous: Tick, previousTrend: Trend): Trend {
+    if (!(current && previous)) {
+        return null;
+    }
+    if (current.lastPrice > previous.lastPrice) {
+        return Trend.UP;
+    } else if (current.lastPrice < previous.lastPrice) {
+        return Trend.DOWN;
+    }
+    return previousTrend;
+}
+
